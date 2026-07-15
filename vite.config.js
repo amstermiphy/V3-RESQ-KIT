63e2442b3603191d4e6081467f967da552be13f8
amstermiphy <zarabiyyu24@gmail.com>
2026-07-15 13:47:52 +0700

    fix: ganti file vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/images/**/*.png",
+                "resources/images/**/*.jpg",
+                "resources/images/**/*.jpeg",
+                "resources/images/**/*.svg",
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
     server: {
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
     },
 });
